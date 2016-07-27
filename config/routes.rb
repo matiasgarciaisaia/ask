ec27c65ffb12509f73f690f2296789ac10c4436d
Mariano Abel Coca <marianoabelcoca@gmail.com>
2016-07-27 13:14:09 -0300

    Fixed #14. Added basic header navigation and flex layout.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :quizzes
   resources :respondents
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "quizzes#index"
 end
